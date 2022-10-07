0643e762e4028276a0beb600060a697ec9ae8e99
Maxim <maxkushuchin@gmail.com>
2022-10-07 17:14:57 +0300

    let's commit

---
+++ 1.py
@@ -0,0 +1,26 @@
+name = "Max"
+surname = "Maggiore"
+born  = 2007
+print (name, "_", surname,"_", born)
+
+name, surname = surname, name
+born += 60
+print (name, surname, born)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
